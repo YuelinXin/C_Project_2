93670a5483741ef934f57a80043d626b707ed85c
YuelinXin <sc20yx2@leeds.ac.uk>
2022-05-06 16:56:57 +0800

    create a shell to auto run the program

--- Makefile
+++ Makefile
@@ -1,20 +1,13 @@
 SRC_DIR = src
 BUILD_DIR = build/debug
 OBJ_NAME = exe
-
 cc = gcc
-
 SRC_FILES = $(wildcard $(SRC_DIR)/*.c)
-
 OBJ_FILES = $(SRC_FILES:.c=.o)
-
 INCLUDE_PATH = -Iinclude
-
 LIB_PATH = -Llib -L/opt/homebrew/lib
-
 COMPILER_FLAGS = -g -Wall -std=c99
-
-LINKER_FLAGS = -lSDL2
+LINKER_FLAGS = -lSDL2 -lSDL2_image
 
 all:
 	$(cc) $(COMPILER_FLAGS) $(INCLUDE_PATH) $(LIB_PATH) $(LINKER_FLAGS) $(SRC_FILES) -o $(BUILD_DIR)/$(OBJ_NAME)
--- src/main.c
+++ src/main.c
@@ -1,16 +1,24 @@
+// ======================================================================
+// run the main program with ```sh run.sh```
+// this command will auto compile the program using Makefile, 
+// and run the program ./build/debug/exe
+// ======================================================================
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 
+// program parameters
+// ======================================================================
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
+// ======================================================================
 
-// ======================================================================
-// run the main program with ./build/debug/exe
-// ======================================================================
 
 int main( void )
 {
@@ -22,6 +30,73 @@ int main( void )
     else
     {
         printf( "SDL initialized!\n" );
+
+        // create window
+        // ======================================================================
+        SDL_Window *window = SDL_CreateWindow( "SDL Test", SDL_WINDOWPOS_CENTERED, 
+            SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN );
+        if ( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            SDL_Quit();
+            return 1;
+        }
+
+        // create renderer
+        // ======================================================================
+        Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+        SDL_Renderer *rend = SDL_CreateRenderer( window, -1, render_flags );
+        if ( !rend )
+        {
+            printf( "error trying to create a renderer: %s\n", SDL_GetError() );
+            SDL_DestroyWindow( window );
+            SDL_Quit();
+            return 1;
+        }
+
+        // create surface
+        // ======================================================================
+        SDL_Surface *surface = IMG_Load( "resources/mf.png" );
+        if ( !surface )
+        {
+            printf( "error creating a surface: %s\n", SDL_GetError() );
+            SDL_DestroyRenderer( rend );
+            SDL_DestroyWindow( window );
+            SDL_Quit();
+            return 1;
+        }
+
+        // create texture
+        // ======================================================================
+        SDL_Texture *texture = SDL_CreateTextureFromSurface( rend, surface );
+        SDL_FreeSurface( surface );
+        if ( !texture )
+        {
+            printf( "error creating a texture: %s\n", SDL_GetError() );
+            SDL_DestroyRenderer( rend );
+            SDL_DestroyWindow( window );
+            SDL_Quit();
+            return 1;
+        }
+
+        // clear the window
+        SDL_RenderClear( rend );
+
+        // draw the image to the window
+        SDL_RenderCopy( rend, texture, NULL, NULL );
+        SDL_RenderPresent( rend );
+
+        // wait a few seconds
+        SDL_Delay( 5000 );
+
+        // clean up resources before exiting
+        SDL_DestroyTexture( texture );
+        SDL_DestroyRenderer ( rend );
+
+        SDL_DestroyWindow( window );
+        SDL_Quit();
+        printf( "Window closed" );
     }
+    // SDL_Quit();
     return 0;
 }
